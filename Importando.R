92ab2508b6ecb39cc66c94dd325f2cddcc46fac6
Izabel Flores <izabelflores9@gmail.com>
2022-03-21 21:28:49 -0300

    Automatizando importação e corrigindo dados faltantes

--- Importando.R
+++ Importando.R
@@ -1,43 +1,51 @@
 
-#%% Censo de 1872 por municipio
+#%% Censo de 1872 por municipio 
 
 library(tidyverse)
-library(readxl)
+library(RODBC)
 
-censo <- read_xlsx("C:/Users/izabe/Desktop/censo 1872 por municipio.xlsx")
+# Importacao
 
-censo2 <- censo %>% 
-  pivot_longer(`Homem livre`:`Total`,
+con <- odbcConnectAccess('Pop-1872-Brasil_versao1_0.mdb')
+censo <- sqlFetch(con,'T8c Consulta_municipios')
+
+# Manipulacao
+
+
+censo2 <- df %>% 
+  pivot_longer(`SomaDeH_LIVRES`:`SomaDeSOMA_G`,
                "Sexo_Condicao")
 
-censo2 <- censo2 %>% 
-  pivot_wider(names_from = c("Grupo de variáveis", "Variável"),
-              values_from = value)
+censo2 <- censo2 %>% pivot_wider(names_from = c('PrimeiroDeGrupo',
+                                                'PrimeiroDeDescrição'),
+                                 values_from = value)
 
-censo2 <- censo2 %>% 
-  group_by(`Código da Província`, 
-           Província, 
-          `Código do Mun.`,
-           Município, 
-          Sexo_Condicao) %>% 
-  summarize_at(vars(`Total_Almas`:`Outras ocupações_S./ Inf.`),
-               sum, na.rm = TRUE)
-
-#%% nomenclatura 
+censo2 <- censo2 %>% group_by(ProvId,
+                              PrimeiroDeProvincia,
+                              MunicId,
+                              PrimeiroDeMunicipio,
+                              Sexo_Condicao) %>%
+  summarize_at(vars(`Total_Almas`:`Outras ocupações_S./ Inf. `), sum, na.rm=TRUE)
 
 censo2 <- censo2 %>% 
   mutate (Sexo_Condicao = case_when(
-    Sexo_Condicao == "Escrava" ~ "Mulher Escravizada",
-    Sexo_Condicao == "Escravo" ~ "Homem Escravizado",
-    Sexo_Condicao == "Homem livre" ~ "Homem Livre",
-    Sexo_Condicao == "Mulher livre" ~ "Mulher Livre",
-    Sexo_Condicao == "Total" ~ "Total",
-    Sexo_Condicao == "Total - escravo" ~ "Total Escravizados",
+    Sexo_Condicao == "SomaDeM_ESCR" ~ "Mulher Escravizada",
+    Sexo_Condicao == "SomaDeH_ESCR" ~ "Homem Escravizado",
+    Sexo_Condicao == "SomaDeH_LIVRES" ~ "Homem Livre",
+    Sexo_Condicao == "SomaDeM_LIVRES" ~ "Mulher Livre",
+    Sexo_Condicao == "SomaDeSOMA_G" ~ "Total",
+    Sexo_Condicao == "SomaDeSOMA_H" ~ "Total Escravizados",
     TRUE ~ "Total Livres"
   )) %>% 
-  arrange(Província, Município, Sexo_Condicao)
+  arrange(PrimeiroDeProvincia, 
+          PrimeiroDeMunicipio, 
+          Sexo_Condicao)
 
-#%% salvando csv
 
-write.csv(censo2, "C:/Users/izabe/Desktop/Github/Censo_1872/Censo_1872_dados_tidy.csv")
+censo2 <- censo2 %>% arrange(PrimeiroDeProvincia, 
+                             PrimeiroDeMunicipio)
+
+#%% Salvando csv
+
+write.csv2(censo2, "C:/Users/izabe/Desktop/Github/Censo_1872/Censo_1872_dados_tidy_versao2.csv")
 
